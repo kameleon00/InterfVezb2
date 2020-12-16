7dc1e4c1966aa08d5da5148f0e7ef70b8bccea6f
Marko Djokic <marko1319@its.edu.rs>
2020-12-16 17:21:24 +0100

    Add project files.

---
+++ InterfVezb2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfVezb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleClass obj = new SimpleClass();
+            //obj.Paint(); // Greška pri kompajliranju.
+            Icontrol c = (Icontrol)obj;
+            c.Print();
+
+            Isurface s = (Isurface)obj;
+            s.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InterfVezb2/SimpleClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfVezb2
+{
+    class SimpleClass : Icontrol, Isurface
+    {
+        void Icontrol.Print()
+        {
+            Console.WriteLine("Icontrol print");
+        }
+        void Isurface.Print()
+        {
+            Console.WriteLine("Isurface print");
+        }
+    }
+}
